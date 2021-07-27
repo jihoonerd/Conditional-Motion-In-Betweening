a878e8b1a5f385821042278b80f0e90e1b149d00
Jihoon Kim <jihoonerd@outlook.com>
2021-07-27 18:24:07 +0900

    Fix sign of NLL

--- rmi/model/utils.py
+++ rmi/model/utils.py
@@ -9,7 +9,7 @@ class NormalNLLLoss:
     """
     def __call__(self, x, mu, var):
         
-        logli = -0.5 * (var.mul(2 * np.pi) + 1e-6).log() - (x - mu).pow(2).div(var.mul(2.0) + 1e-6)
+        logli = -0.5 * (var.mul(2 * np.pi) + 1e-6).log() + (x - mu).pow(2).div(var.mul(2.0) + 1e-6)
         nll = -(logli.sum(1).mean())
 
         return nll
