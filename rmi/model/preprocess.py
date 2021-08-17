45a60c75e51f8401301ce9fd088fffff07d6d1d7
Jihoon Kim <jihoonerd@outlook.com>
2021-08-17 18:27:12 +0900

    Add create_mask function and test

--- rmi/model/preprocess.py
+++ rmi/model/preprocess.py
@@ -13,3 +13,17 @@ def vectorize_pose(sampled_batch):
 
     out = torch.cat([root_p, local_q], dim=2)
     return out
+
+
+def create_mask(pose_vector, device, mask_start=10, mask_end=49):
+    """Generate masks for pose vector. Masking range is [mask_start, mask_end)
+
+    Returns: src_mask, src_padding_mask
+    """
+    
+    seq_len = pose_vector.shape[0]
+    batch_size = pose_vector.shape[1]
+    src_mask = torch.zeros((seq_len, seq_len), device=device).type(torch.bool)  # (seq_len, seq_len)
+    src_mask[:, mask_start:mask_end] = True
+    src_padding_mask = torch.zeros((batch_size, seq_len), device=device).type(torch.bool)
+    return src_mask, src_padding_mask
--- tests/test_proprocess.py
+++ tests/test_proprocess.py
@@ -1,6 +1,8 @@
-from rmi.model.preprocess import vectorize_pose
+from rmi.model.preprocess import vectorize_pose, create_mask
 import numpy as np
 import torch
+
+device = torch.device("cpu")
 
 def test_vectorize_pose():
     pose_local_q = torch.randn((64, 50, 22, 4))
@@ -15,3 +17,22 @@ def test_vectorize_pose():
     np.testing.assert_array_equal(pose_vec[:,:,:3].permute(1,0,2), pose_root_p)
     np.testing.assert_array_equal(pose_vec[:,:,3:].permute(1,0,2).reshape(64, 50, 22, 4), pose_local_q)
     np.testing.assert_equal(pose_vec.shape, (50, 64, 91))
+
+
+def test_create_mask():
+
+    pose_local_q = torch.randn((64, 50, 22, 4))
+    pose_root_p = torch.randn((64, 50, 3))
+    sampled_batch = {
+        'local_q': pose_local_q,
+        'root_p': pose_root_p
+    }
+    
+    pose_vec = vectorize_pose(sampled_batch)
+    src_mask1, src_padding_mask1 = create_mask(pose_vec, device=device, mask_start=10, mask_end=49)
+    assert (src_mask1[:,10:49] == True).all()
+    assert (src_padding_mask1 == False).all()
+    
+    src_mask2, src_padding_mask2 = create_mask(pose_vec, device=device, mask_start=20, mask_end=45)
+    assert (src_mask2[:,20:45] == True).all()
+    assert (src_padding_mask2 == False).all()
