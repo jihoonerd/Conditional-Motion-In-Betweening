a1057be295d79ec0bb91f90ff9f1320902cbd16d
Jihoon Kim <jihoonerd@outlook.com>
2021-08-24 23:26:12 +0900

    Infogan -CR

--- test_tfmodel.py
+++ test_tfmodel.py
@@ -29,7 +29,7 @@ def test(opt, device):
 
     # Load LAFAN Dataset
     Path(opt.processed_data_dir).mkdir(parents=True, exist_ok=True)
-    lafan_dataset = LAFAN1Dataset(lafan_path=opt.data_path, processed_data_dir=opt.processed_data_dir, train=False, target_action=['walk'], device=device, start_seq_length=30, cur_seq_length=30, max_transition_length=30)
+    lafan_dataset = LAFAN1Dataset(lafan_path=opt.data_path, processed_data_dir=opt.processed_data_dir, train=False, target_action=['walk', 'dance'], device=device, start_seq_length=30, cur_seq_length=30, max_transition_length=30)
     
     # LERP In-betweening Frames
     from_idx, target_idx = 9, 39
@@ -37,8 +37,8 @@ def test(opt, device):
     root_lerped, local_q_lerped = lerp_pose(lafan_dataset.data, from_idx=from_idx, target_idx=target_idx)
     contact_init = torch.ones(lafan_dataset.data['contact'].shape) * 0.5
 
-    pose_vectorized_gt = vectorize_pose(lafan_dataset.data['root_p'], lafan_dataset.data['local_q'], lafan_dataset.data['contact'], 96, device)[:,from_idx:target_idx,:]
-    pose_vectorized_lerp = vectorize_pose(root_lerped, local_q_lerped, contact_init, 96, device)[:,from_idx:target_idx,:]
+    pose_vectorized_gt = vectorize_pose(lafan_dataset.data['root_p'], lafan_dataset.data['local_q'], lafan_dataset.data['contact'], 100, device)[:,from_idx:target_idx,:]
+    pose_vectorized_lerp = vectorize_pose(root_lerped, local_q_lerped, contact_init, 100, device)[:,from_idx:target_idx,:]
 
     # Extract dimension from processed data
     root_v_dim = lafan_dataset.root_v_dim
@@ -52,9 +52,12 @@ def test(opt, device):
     src_mask = torch.zeros((horizon, horizon), device=device).type(torch.bool)
     src_mask = src_mask.to(device)
 
+    infogan_code_gen = torch.zeros(pose_vectorized_lerp.shape[1], 2)
+    pose_vectorized_lerp[:,:,repr_dim: repr_dim + 2] = infogan_code_gen
+
     test_idx = [4,5,6]
 
-    model = TransformerModel(seq_len=horizon, d_model=96, nhead=8, d_hid=1024, nlayers=8, dropout=0.05, out_dim=repr_dim, device=device)
+    model = TransformerModel(seq_len=horizon, d_model=100, nhead=10, d_hid=1024, nlayers=8, dropout=0.05, out_dim=repr_dim, device=device)
     model.load_state_dict(ckpt['transformer_encoder_state_dict'])
     model.eval()
 
@@ -109,10 +112,10 @@ def test(opt, device):
 def parse_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument('--project', default='runs/train', help='project/name')
-    parser.add_argument('--ckpt_path', type=str, default='train-2800.pt', help='weights path')
+    parser.add_argument('--ckpt_path', type=str, default='train-200.pt', help='weights path')
     parser.add_argument('--data_path', type=str, default='ubisoft-laforge-animation-dataset/output/BVH', help='BVH dataset path')
     parser.add_argument('--skeleton_path', type=str, default='ubisoft-laforge-animation-dataset/output/BVH/walk1_subject1.bvh', help='path to reference skeleton')
-    parser.add_argument('--processed_data_dir', type=str, default='processed_data_walk/', help='path to save pickled processed data')
+    parser.add_argument('--processed_data_dir', type=str, default='processed_data_walk_dance/', help='path to save pickled processed data')
     parser.add_argument('--save_path', type=str, default='runs/test', help='path to save model')
     opt = parser.parse_args()
     return opt
--- train.sh
+++ train.sh
@@ -1,14 +1,19 @@
-python3 train_tfmodel.py \
-    --processed_data_dir='processed_data_walk/' \
-	--batch_size=16 \
+python train_tfmodel.py \
+	--processed_data_dir="processed_data_walk_dance/" \
+	--batch_size=8 \
 	--epochs=5000 \
-	--device=1 \
+	--device=2 \
 	--entity=rilab-motion \
-	--exp_name="WALK: bert (llr, lqc0.2, AttAll)" \
-	--save_interval=200 \
-	--generator_learning_rate=0.00001 \
+	--exp_name="WALK_DANCE: BERT ICR (8, GANONLY)" \
+	--save_interval=50 \
+	--generator_learning_rate=0.001 \
+	--discriminator_learning_rate=0.00001 \
+	--cr_learning_rate=0.001 \
 	--optim_beta1=0.5 \
-	--optim_beta2=0.9 \
-	--loss_root_weight=0.01 \
-	--loss_quat_weight=1.0 \
-	--loss_contact_weight=0.2
+	--optim_beta2=0.99 \
+	--loss_discriminator_weight=0.8 \
+	--loss_gan_weight=1.0 \
+	--loss_code_weight=1.0 \
+	--infogan_disc_code=2 \
+	--infogan_cont_code=0 \
+	--loss_crh_weight=0.2
