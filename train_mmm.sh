30e4ec6773cafe3d001a25792fe687a4971cb61c
Jihoon Kim <jihoonerd@outlook.com>
2021-09-16 14:51:21 +0900

    Trivial update
    Former-commit-id: 2d55c7f64652004808fc01aac85ead4fac7938a8 [formerly 2d55c7f64652004808fc01aac85ead4fac7938a8 [formerly 2eb8c9b0bd4bc259dfda9d1171703672ad142474]]
    Former-commit-id: 8bd60fe9f30215c5ea84908a9d01ef89493bd6ec
    Former-commit-id: 81aba931ffb61e2a8e554bfc58fba726d7230c09

--- train_mmm.sh
+++ train_mmm.sh
@@ -2,9 +2,9 @@ python train_mmm.py \
 	--processed_data_dir="processed_data_original/" \
 	--batch_size=1024 \
 	--epochs=10000 \
-	--device=2 \
+	--device=3 \
 	--entity=rilab-motion \
-	--exp_name="Link-NP-Global_003" \
+	--exp_name="preserve_link_pp" \
 	--save_interval=100 \
 	--learning_rate=0.0001 \
 	--optim_beta1=0.9 \
@@ -12,5 +12,5 @@ python train_mmm.py \
 	--loss_pos_weight=0.03 \
 	--loss_rot_weight=1.0 \
 	--from_idx=9 \
-	--target_idx=40 \
-	--preserve_link_train
+	--target_idx=40
+	# --preserve_link_train
