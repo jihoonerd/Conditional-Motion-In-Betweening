a2ef65ef59b934f8235005dfdccc9cbdf858847e
Jihoon Kim <jihoonerd@outlook.com>
2021-09-25 00:34:45 +0900

    Add slerp
    Former-commit-id: 7e5f5bb6d66bd8358f5a97532be9e85ddf12c939 [formerly 7e5f5bb6d66bd8358f5a97532be9e85ddf12c939 [formerly 33388c4dfe2235401e7b02bee84df8fa7ee72d74]]
    Former-commit-id: 9a3d73fb01086164c3a8d02ce6e066c8bc39e5eb
    Former-commit-id: ac25bd9aef216a1e6e1ee524132009caf2523ad8

--- rmi/model/preprocess.py
+++ rmi/model/preprocess.py
@@ -87,20 +87,81 @@ def lerp_reshaped(input_vec, mask_start_frame, num_offsets):
     return interpolated.reshape(batch_size, seq_len, -1)
 
 
-def slerp_input_repr(low, high, t):
-    # low, high: [N, 22, 3]
+def slerp(x, y, a):
+    """
+    Perfroms spherical linear interpolation (SLERP) between x and y, with proportion a
 
-    low_norm = torch.nn.functional.normalize(low, p=2.0, dim=2)
-    high_norm = torch.nn.functional.normalize(high, p=2.0, dim=2)
-    omega = torch.acos((low_norm * high_norm).sum(2)) # (Batch, 22)
-    so = torch.sin(omega)
-    res = (torch.sin((1.0-t) * omega)/so).unsqueeze(2) * low + (torch.sin(t * omega)/so).unsqueeze(2) * high
+    :param x: quaternion tensor
+    :param y: quaternion tensor
+    :param a: indicator (between 0 and 1) of completion of the interpolation.
+    :return: tensor of interpolation results
+    """
+    device = x.device
+    len = torch.sum(x * y, dim=-1)
+
+    neg = len < 0.0
+    len[neg] = -len[neg]
+    y[neg] = -y[neg]
+
+    a = torch.zeros_like(x[..., 0]) + a
+    amount0 = torch.zeros(a.shape, device=device)
+    amount1 = torch.zeros(a.shape, device=device)
+
+    linear = (1.0 - len) < 0.01
+    omegas = torch.arccos(len[~linear])
+    sinoms = torch.sin(omegas)
+
+    amount0[linear] = 1.0 - a[linear]
+    amount0[~linear] = torch.sin((1.0 - a[~linear]) * omegas) / sinoms
+
+    amount1[linear] = a[linear]
+    amount1[~linear] = torch.sin(a[~linear] * omegas) / sinoms
+    # res = amount0[..., np.newaxis] * x + amount1[..., np.newaxis] * y
+    res = amount0.unsqueeze(3) * x + amount1.unsqueeze(3) * y
+
     return res
 
-def lerp_input_repr(minibatch_pose_input, mask_start_frame):
-    # Use LERP for positions and SLERP for rotations
-    # minibatch_pose_input (N.L.D)
+def slerp_input_repr(minibatch_pose_input, mask_start_frame):
+    seq_len = minibatch_pose_input.size(1)
+    minibatch_pose_input = minibatch_pose_input.reshape(minibatch_pose_input.size(0), seq_len, -1, 4)
+    interpolated = torch.zeros_like(minibatch_pose_input, device=minibatch_pose_input.device)
 
+    if mask_start_frame == 0 or mask_start_frame == (seq_len -1):
+        interpolate_start = minibatch_pose_input[:,0:1]
+        interpolate_end = minibatch_pose_input[:,seq_len-1:]
+
+        for i in range(seq_len):
+            dt = 1 / (seq_len-1)
+            interpolated[:,i:i+1,:] = slerp(interpolate_start, interpolate_end, dt * i)
+
+        assert torch.allclose(interpolated[:,0:1], interpolate_start)
+        assert torch.allclose(interpolated[:,seq_len-1:], interpolate_end)
+    else:
+        interpolate_start1 = minibatch_pose_input[:,0:1]
+        interpolate_end1 = minibatch_pose_input[:,mask_start_frame:mask_start_frame+1]
+
+        interpolate_start2 = minibatch_pose_input[:, mask_start_frame:mask_start_frame+1]
+        interpolate_end2 = minibatch_pose_input[:,seq_len-1:]
+
+        for i in range(mask_start_frame+1):
+            dt = 1 / mask_start_frame
+            interpolated[:,i:i+1,:] = slerp(interpolate_start1, interpolate_end1, dt * i)
+
+        assert torch.allclose(interpolated[:,0:1], interpolate_start1)
+        assert torch.allclose(interpolated[:,mask_start_frame:mask_start_frame+1], interpolate_end1)
+        
+        for i in range(mask_start_frame, seq_len):
+            dt = 1 / (seq_len - mask_start_frame - 1)
+            interpolated[:,i:i+1,:] = slerp(interpolate_start2, interpolate_end2, dt * (i - mask_start_frame))
+        
+        assert torch.allclose(interpolated[:,mask_start_frame:mask_start_frame+1], interpolate_start2)
+        assert torch.allclose(interpolated[:,seq_len-1:], interpolate_end2)
+
+    interpolated = torch.nn.functional.normalize(interpolated, p=2.0, dim=3)
+    return interpolated.reshape(minibatch_pose_input.size(0), seq_len, -1)
+
+
+def lerp_input_repr(minibatch_pose_input, mask_start_frame):
     seq_len = minibatch_pose_input.size(1)
     interpolated = torch.zeros_like(minibatch_pose_input, device=minibatch_pose_input.device)
 
