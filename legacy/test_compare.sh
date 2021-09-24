4da46a4c1aecdc1ab3d089c8788fed34d5c1eee7
Jihoon Kim <jihoonerd@outlook.com>
2021-09-24 20:47:45 +0900

    Drop artifacts
    Former-commit-id: cda48c2e973af63c4163a28532f8b36dc5c84dfe [formerly cda48c2e973af63c4163a28532f8b36dc5c84dfe [formerly 08064d99ccff4124339a7ebd2697a50d1774f22c]]
    Former-commit-id: 7cecd143823572072de27bebc81d966fb4613637
    Former-commit-id: 3ba872d3915f719d11b3187faf7eaeadf467d490

--- legacy/test_compare.sh
+++
@@ -1,7 +0,0 @@
-python3 test_compare_plots.py \
-	--pretrained_weights="runs/train/hard-injecting-wdj2/weights/train-17.pt" \
-	--infogan_disc_code=3 \
-	--infogan_cont_code=2 \
-	--exp_name=D2C1 \
-	# --control_latent='cond' \
-	--processed_data_dir="processed_data_walk_dance_jump/"
