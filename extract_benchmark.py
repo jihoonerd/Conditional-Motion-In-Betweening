6307376236a1b611b4e08638b521c728101171be
Jihoon Kim <jihoonerd@outlook.com>
2021-10-27 16:18:53 +0900

    Drop legacy

--- extract_benchmark.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-
-if __name__ == "__main__":
-
-    for i in range(300, 7201, 25):
-        print(i)
-        subprocess.run(["python", "test_benchmark.py", "--weight", str(i)])  # doesn't capture output
